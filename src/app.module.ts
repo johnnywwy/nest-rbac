1341276328ebf4ac4822406c950d9b4ce595fe9d
wuwenyu <johnnywwy@gmail.com>
2024-05-31 17:02:02 +0800

    feat: 创建user模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: true, //todo 生产环境请关闭
       logging: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
