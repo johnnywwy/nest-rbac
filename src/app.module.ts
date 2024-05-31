2f5a3136b98aa62eec2653e81668c3ea21ca58bb
wuwenyu <johnnywwy@gmail.com>
2024-05-31 16:49:55 +0800

    chore: 数据库连接成功

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3308,
+      username: 'root',
+      password: '123456',
+      database: 'nest_rbac_test',
+      autoLoadEntities: true,
+      synchronize: true, //todo 生产环境请关闭
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
