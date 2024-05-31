251999fc05c02d3f585e05f609ae8b74e069e53f
wuwenyu <johnnywwy@gmail.com>
2024-05-31 17:34:02 +0800

    format：格式化

--- src/modules/role/entities/role.entity.ts
+++ src/modules/role/entities/role.entity.ts
@@ -1,1 +1,18 @@
-export class Role {}
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany } from 'typeorm';
+import { Permission } from './permission.entity';
+import { Menu } from '../../';
+
+@Entity()
+export class Role {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @ManyToMany(() => Permission)
+  permissions: Permission[];
+
+  @ManyToMany(() => Menu)
+  menus: Menu[];
+}
--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
+import { Role } from '../../role/entities/role.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @ManyToMany(() => Role)
+  @JoinTable()
+  roles: Role[];
+}
