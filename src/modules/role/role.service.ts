d99b926e59cd0fedecc40588c6d9503e396ebf83
wuwenyu <johnnywwy@gmail.com>
2024-05-31 18:34:07 +0800

    feat:Role 创建接口

--- src/modules/role/role.service.ts
+++ src/modules/role/role.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Role } from './entities/role.entity';
 
 @Injectable()
 export class RoleService {
+  constructor(
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>,
+  ) {}
   create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+    return this.roleRepository.save(createRoleDto);
   }
 
   findAll() {
