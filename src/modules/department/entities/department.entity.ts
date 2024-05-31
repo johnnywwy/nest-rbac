e092718a0374ecf6bee2e36cbe12ff84036be1cf
wuwenyu <johnnywwy@gmail.com>
2024-05-31 18:10:31 +0800

    feat: Department 先隐藏

--- src/modules/department/entities/department.entity.ts
+++ src/modules/department/entities/department.entity.ts
@@ -1,1 +1,12 @@
-export class Department {}
+// import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+// import { User } from '../../user/entities/user.entity';
+
+// @Entity('department')
+export class Department {
+  // @PrimaryGeneratedColumn()
+  // id: number;
+  // @Column()
+  // name: string;
+  // @ManyToOne(() => User, (user) => user.department)
+  // user: User;
+}
